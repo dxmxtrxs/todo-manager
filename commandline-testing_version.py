935086a6ca53701024cf3edae8e9a64632620194
dxmxtrxs <126823091+dxmxtrxs@users.noreply.github.com>
2023-07-26 10:34:56 +0300

    Update commandline-testing_version.py
    I have added the ability to add a task as well as a sub-task. Furthermore, I added the ability to view the JSON file directly with a print statement in a raw form for testing, and this function can also be used in other functions in order for them to access the JSON file.

--- commandline-testing_version.py
+++ commandline-testing_version.py
@@ -1,1 +1,56 @@
+import json 
+import os
+import tkinter
 
+
+def read_tasks(user):
+    # Checking if the file exists to avoid errors
+    if os.path.isfile("todo.json"):
+        
+        with open("todo.json", "r") as file:
+            try:
+                return json.load(file) # Returning the json file
+            except json.decoder.JSONDecodeError:
+                if user:
+                    return "No tasks to show"
+                else:
+                    return False
+    else:
+        if user:
+            return "No saves"
+        else:
+            return False
+
+
+def add_task(task: str):
+    default = {"status":False}
+    # Checking to see if we should add to an existing file or to add to a new file 
+    # with the inserted task.
+    if read_tasks(False):
+        all_tasks = read_tasks(False)
+        all_tasks[task] = default
+        # Placing the new data in the json file,
+        with open("todo.json", "w") as file:
+            json.dump(all_tasks, file)
+            print("Task added!")
+    else:
+        # In the case of the file not existing the program will create a new json file
+        # to place the task to.
+        print("doesnt exist/empty")
+        with open("todo.json", "w") as file:
+            print("Creating new save")
+            new = {}
+            new[task] = default
+            json.dump(new, file)
+            print("Task added!")
+
+
+def add_subtask(task,subtask):
+    try:
+        all_tasks = read_tasks(False)
+        all_tasks[task][subtask] = False
+        with open("todo.json", "w") as file:
+            json.dump(all_tasks, file)
+            print("Sub task added successfully!")
+    except KeyError:
+        print("The specified task doesnt exist")
